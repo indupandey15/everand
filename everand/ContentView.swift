052f7ab3066f77da321636ed540e4b17d9aba668
Indu Pandey <indupandey0105@gmail.com>
2024-03-29 21:02:46 -0400

    Initial commit

---
+++ everand/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  everand
+//
+//  Created by Indu Pandey on 29/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ everand/everandApp.swift
@@ -0,0 +1,17 @@
+//
+//  everandApp.swift
+//  everand
+//
+//  Created by Indu Pandey on 29/03/24.
+//
+
+import SwiftUI
+
+@main
+struct everandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
